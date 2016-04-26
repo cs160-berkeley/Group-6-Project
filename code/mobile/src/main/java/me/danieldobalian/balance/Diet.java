66acd4d28d0663af6423831a587f3f7244af07af
Kevin Baum <link.kb@gmail.com>
2016-04-25 21:56:17 -0700

    added data crunch and helper methods for Diet and Light to Diet.java

--- code/mobile/src/main/java/me/danieldobalian/balance/Diet.java
+++ code/mobile/src/main/java/me/danieldobalian/balance/Diet.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import java.lang.Math.*;
 
 public class Diet extends AppCompatActivity {
 
@@ -28,6 +29,87 @@ public class Diet extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
+
+    protected void checkAlcoholAbuse() {
+        boolean past3DaysAlcohol = false; //this should be read from history
+        if (past3DaysAlcohol) {
+            //queue alcohol abuse warning notification
+        }
+    }
+
+    protected void checkHealthReward() {
+        int healthyStreak = 2; //this should be read from history.
+        switch(healthyStreak){
+            case 2: break; //queue 3 days healthy notification
+            case 6: break; //queue 1 week healthy notification
+            case 13: break; //queue 2 weeks healthy notification
+            case 30: break; //queue 1 month healthy notification
+        }
+    }
+
+    protected double dietDataCrunch(boolean inputs[]) {
+        /* let inputs be as follows:
+        0 = veggies
+        1 = fruits
+        2 = meats
+        3 = sugar
+        4 = fat
+        5 = caffine
+        6 = alcohol
+        */
+
+        double stars;
+        stars = 0;
+        for (int i=0;i<7;i++) {
+            if (i<4) {stars+=2;}
+            else {stars--;}
+        }
+        stars = Math.max((double) 0, stars);
+        stars = Math.min((double) 5,stars);
+
+        //check for alcohol abuse
+        boolean alcohol = inputs[6];
+        if(alcohol) {checkAlcoholAbuse();}
+
+        //check for health rewards
+        if(stars==(double)5) {checkHealthReward();}
+
+        return stars*20;
+    };
+
+    protected void checkHomebound() {
+        boolean past2DaysHomebound = false; //this should be read from history
+        if (past2DaysHomebound) {
+            //queue a lack of going outside notification
+        }
+    }
+
+    protected void checkExerciseReward() {
+        int exerciseStreak = 2; //this should be read from history
+        switch(exerciseStreak){
+            case 2: break; //queue 3 days exercising notification
+            case 6: break; //queue 1 week exercising notification
+            case 13: break; //queue 2 weeks exercising notification
+            case 30: break; //queue 1 month exercising notification
+        }
+    }
+
+    protected double lightDataCrunch(int[] light) {
+        int timeIncrement = 15; //how frequently the sensor checks
+        int sunlight = 0;
+        for (int i=0; i<light.length; i++) {
+            if (light[i]>50) { //the threshhold for being outside
+                sunlight+=timeIncrement;
+            }
+        }
+        if (sunlight==0) {
+            checkHomebound();
+        }
+        if (sunlight>=30) {
+            checkExerciseReward();
+        }
+        return (double) Math.max((double)100,50*sunlight/30);
     }
 
 }

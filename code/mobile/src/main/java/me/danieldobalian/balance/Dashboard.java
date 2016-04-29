ade631c7fac943b22ea7c4475a7f8a542f19a6bd
Kevin Baum <link.kb@gmail.com>
2016-04-29 01:58:58 -0700

    notification buttons on dashboard working; functional helper method as well, see button 2.

--- code/mobile/src/main/java/me/danieldobalian/balance/Dashboard.java
+++ code/mobile/src/main/java/me/danieldobalian/balance/Dashboard.java
@@ -1,15 +1,19 @@
 package me.danieldobalian.balance;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -91,5 +95,23 @@ public class Dashboard extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View view) {
+        int notificationId = 001;
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.logobg)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.logobg))
+                        .setContentTitle("Balance")
+                        .setContentText("Seems like you're having a good day; keep it up!");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    public void sendNotification(View view) {
+        String title = "Balance";
+        String text = "Hey, is everything going okay? Just checking in!";
+        NotificationHelper.sendTextNotification(view, title, text, this);
     }
 }
---
+++ code/mobile/src/main/java/me/danieldobalian/balance/NotificationHelper.java
@@ -0,0 +1,34 @@
+package me.danieldobalian.balance;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
+import android.content.Intent;
+
+/**
+ * Created by Kevin on 4/29/2016.
+ */
+public class NotificationHelper {
+    public static void sendTextNotification(View view, String title, String text, Context caller) {
+        int notificationId = 002;
+        Intent viewIntent = new Intent(caller, StartScreen.class);
+        PendingIntent viewPendingIntent = PendingIntent.getActivity(caller, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(caller)
+                        .setSmallIcon(R.mipmap.logobg)
+                        .setLargeIcon(BitmapFactory.decodeResource(view.getResources(), R.mipmap.logobg))
+                        .setContentTitle(title)
+                        .setContentText(text);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(caller);
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+}

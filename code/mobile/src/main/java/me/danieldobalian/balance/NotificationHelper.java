295f0501eb08529688bdb973cf075d1722c544bd
danieldobalian <ddobalian@gmail.com>
2016-04-30 17:46:48 -0700

    Added imports to Kevin's notification code

--- code/mobile/src/main/java/me/danieldobalian/balance/NotificationHelper.java
+++ code/mobile/src/main/java/me/danieldobalian/balance/NotificationHelper.java
@@ -3,6 +3,9 @@ package me.danieldobalian.balance;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.view.View;
 
 /**

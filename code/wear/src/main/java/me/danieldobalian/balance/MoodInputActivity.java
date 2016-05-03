d1795d32c51c3a2fca1e2ffdc4afda6da82a6dd0
Hamza Hashmi <hamzahashmi786@gmail.com>
2016-05-02 23:27:37 -0700

    fixed watch mood UI

--- code/wear/src/main/java/me/danieldobalian/balance/MoodInputActivity.java
+++ code/wear/src/main/java/me/danieldobalian/balance/MoodInputActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,26 +13,18 @@ public class MoodInputActivity extends Activity {
 
     private TextView mTextView;
     SeekBar scale;
-
+    Button input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_input);
-        scale = (SeekBar) findViewById(R.id.seek);
-        scale.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        scale = (SeekBar) findViewById(R.id.happyslide);
+        input = (Button) findViewById(R.id.input);
+
+        input.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onClick(View v) {
 
                 int mood_scale_value = scale.getProgress();
                 String mood_val = Integer.toString(mood_scale_value);
@@ -46,9 +40,40 @@ public class MoodInputActivity extends Activity {
                 Intent intent= new Intent(getBaseContext(), DataInputtedActivity.class);
                 startActivity(intent);
 
-
             }
         });
+
+//         scale.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//                int mood_scale_value = scale.getProgress();
+//                String mood_val = Integer.toString(mood_scale_value);
+//
+//                Log.v("T", "Sending mood to watch2phone");
+//                Intent sendIntent = new Intent(getBaseContext(), WatchToPhoneService.class);
+//                sendIntent.putExtra("command", "mood");
+//                sendIntent.putExtra("data", mood_val);
+//                startService(sendIntent);
+//
+//
+//
+//                Intent intent= new Intent(getBaseContext(), DataInputtedActivity.class);
+//                startActivity(intent);
+//
+//
+//            }
+//        });
 
 
 //        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
